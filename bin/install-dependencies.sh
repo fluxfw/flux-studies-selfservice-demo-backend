1598642b3b03becc97d9522cfb33083f373029e0
fluxfw <fluxfw@fluxlabs.ch>
2022-10-17 12:00:27 +0200

    Dynamic imports

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -8,8 +8,8 @@ else
     node_modules="node_modules"
 fi
 
-(mkdir -p $node_modules/flux-express-server-api && cd $node_modules/flux-express-server-api && wget -O - https://github.com/fluxfw/flux-express-server-api/archive/refs/tags/v2022-10-03-1.tar.gz | tar -xz --strip-components=1)
+(mkdir -p $node_modules/flux-express-server-api && cd $node_modules/flux-express-server-api && wget -O - https://github.com/fluxfw/flux-express-server-api/archive/refs/tags/v2022-10-17-1.tar.gz | tar -xz --strip-components=1)
 
-(mkdir -p $node_modules/flux-shutdown-handler-api && cd $node_modules/flux-shutdown-handler-api && wget -O - https://github.com/fluxfw/flux-shutdown-handler-api/archive/refs/tags/v2022-10-03-1.tar.gz | tar -xz --strip-components=1)
+(mkdir -p $node_modules/flux-shutdown-handler-api && cd $node_modules/flux-shutdown-handler-api && wget -O - https://github.com/fluxfw/flux-shutdown-handler-api/archive/refs/tags/v2022-10-17-1.tar.gz | tar -xz --strip-components=1)
 
-(mkdir -p $node_modules/flux-studies-selfservice-frontend && cd $node_modules/flux-studies-selfservice-frontend && wget -O - https://github.com/fluxfw/flux-studies-selfservice-frontend/archive/refs/tags/v2022-10-14-1-alpha.tar.gz | tar -xz --strip-components=1 && npm ci --omit=dev)
+(mkdir -p $node_modules/flux-studies-selfservice-frontend && cd $node_modules/flux-studies-selfservice-frontend && wget -O - https://github.com/fluxfw/flux-studies-selfservice-frontend/archive/refs/tags/v2022-10-17-1-alpha.tar.gz | tar -xz --strip-components=1 && npm ci --omit=dev)
