86ff6460ab9e189ed909929e21e43d9f5f0200c3
fluxfw <fluxfw@fluxlabs.ch>
2022-11-01 15:49:47 +0100

    Crop photo

--- src/Adapter/Application/Application.mjs
+++ src/Adapter/Application/Application.mjs
@@ -1,5 +1,5 @@
-/** @typedef {import("../../../node_modules/flux-studies-selfservice-frontend/src/Adapter/Page/PAGE.mjs").PAGE} PAGE */
-/** @typedef {import("../../../node_modules/flux-studies-selfservice-frontend/src/Adapter/Post/Post.mjs").Post} Post */
+/** @typedef {import("../../../../flux-studies-selfservice-frontend/src/Adapter/Page/PAGE.mjs").PAGE} PAGE */
+/** @typedef {import("../../../../flux-studies-selfservice-frontend/src/Adapter/Post/Post.mjs").Post} Post */
 
 /**
  * @typedef {{"identification-number": string, page: PAGE, posts: Post[]}} Application
