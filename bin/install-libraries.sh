6dabc560b059d3716e43d41bdec18a6ccdadc0b8
fluxfw <fluxfw@fluxlabs.ch>
2023-01-03 10:43:14 +0100

    Fix fallback languages / `flux-http-api` / Reduce `express`

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -26,12 +26,10 @@ installNodeModules
 
 installLibrary flux-config-api https://github.com/fluxfw/flux-config-api/archive/refs/tags/v2022-12-08-1.tar.gz
 
-installLibrary flux-express-server-api https://github.com/fluxfw/flux-express-server-api/archive/refs/tags/v2022-12-08-1.tar.gz
+installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-01-03-1.tar.gz
 
-installLibrary flux-fetch-api https://github.com/fluxfw/flux-fetch-api/archive/refs/tags/v2022-12-12-1.tar.gz
-
-installLibrary flux-json-api https://github.com/fluxfw/flux-json-api/archive/refs/tags/v2022-12-08-1.tar.gz
+installLibrary flux-json-api https://github.com/fluxfw/flux-json-api/archive/refs/tags/v2023-01-03-1.tar.gz
 
 installLibrary flux-shutdown-handler-api https://github.com/fluxfw/flux-shutdown-handler-api/archive/refs/tags/v2022-12-08-1.tar.gz
 
-installLibrary flux-studis-selfservice-frontend/src https://github.com/fluxfw/flux-studis-selfservice-frontend/releases/download/v2022-12-22-1/flux-studis-selfservice-frontend-v2022-12-22-1-build.tar.gz
+installLibrary flux-studis-selfservice-frontend/src https://github.com/fluxfw/flux-studis-selfservice-frontend/releases/download/v2023-01-03-1/flux-studis-selfservice-frontend-v2023-01-03-1-build.tar.gz
