8568350eaa5198d84b47e1e677f31417d4d31c46
fluxfw <fluxfw@fluxlabs.ch>
2023-06-15 14:48:39 +0200

    `flux-form`

--- bin/install-libraries.sh
+++ bin/install-libraries.sh
@@ -21,7 +21,7 @@ checkAlreadyInstalled
 
 installLibrary flux-config-api https://github.com/fluxfw/flux-config-api/archive/refs/tags/v2023-06-08-1.tar.gz
 
-installLibrary flux-form https://github.com/fluxfw/flux-form/archive/refs/tags/v2023-06-09-1.tar.gz
+installLibrary flux-form https://github.com/fluxfw/flux-form/archive/refs/tags/v2023-06-15-1.tar.gz
 
 installLibrary flux-http-api https://github.com/fluxfw/flux-http-api/archive/refs/tags/v2023-04-20-1.tar.gz
 
