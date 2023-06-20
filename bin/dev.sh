c02eec7de3098885126a7d19ec80701bb0204935
fluxfw <fluxfw@fluxlabs.ch>
2023-06-20 15:24:32 +0200

    dev

---
+++ bin/dev.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+set -e
+
+host_ip="$1"
+if [ -z "$host_ip" ]; then
+    echo "Please pass a host ip" >&2
+    exit 1
+fi
+
+bin="`dirname "$0"`"
+root="$bin/.."
+libs="$root/.."
+
+name="`basename "$(realpath "$root")"`"
+user="${FLUX_PUBLISH_DOCKER_USER:=fluxfw}"
+image="$user/$name"
+tag="`get-release-tag "$root"`"
+
+(cd "$libs" && node-docker flux-studis-selfservice-frontend/bin/generate-pwa.mjs)
+
+xdg-open "http://$host_ip"
+
+docker run --rm -v "$libs:/$name/libs:ro" -p "$host_ip:80:80" "$image:$tag"
+
+#git-clean-ignored "$root"
